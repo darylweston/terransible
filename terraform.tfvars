29c618e48a1154f66bd142c1046c3500c26f5e7f
Daryl <daryl@Daryls-MBP.lan>
2020-05-21 16:05:32 +0100

    new-files

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
-aws_profile = "superhero"
 aws_region  = "eu-west-2"
+aws_profile = "superhero"
+vpc_cidr    = "10.0.0.0/16"
--- variables.tf
+++ variables.tf
@@ -1,2 +1,5 @@
 variable "aws_region" {}
 variable "aws_profile" {}
+variable "vpc_cidr" {}
+
+# data "aws_availability_zones" "available"
