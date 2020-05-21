c69e957ff2c76405ea4082e1bf5636049775e6f0
Daryl <daryl@Daryls-MBP.lan>
2020-05-21 15:14:35 +0100

    new-files

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  reigion = var.aws_region
+  profile = var.aws_profile
+}
+
+
+#-----IAM------
+
+#s3_access
+
+resource "aws_iam_instance_profile" "s3_access_profile" {
+  name = "s3_access"
+  role = "${aws_iam_role.s3_access_role.name}"
+}
+
+resource "aws_iam_role_policy" "s3_access_policy" {
+  name = "s3_access_policy"
+  role = "${aws_iam_role.s3_access_role.id}"
+}
