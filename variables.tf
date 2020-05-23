a5ba0d400ca03b19b41ec41f23dab5b57568e13f
Daryl <daryl@Daryls-MBP.lan>
2020-05-23 13:15:31 +0100

    new-files

--- variables.tf
+++ variables.tf
@@ -9,3 +9,7 @@ variable "db_instance_class" {}
 variable "dbname" {}
 variable "dbuser" {}
 variable "dbpassword" {}
+variable "key_name" {}
+variable "public_key_path" {}
+variable "dev_ami" {}
+variable "dev_instance_type" {}
