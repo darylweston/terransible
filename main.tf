5c92e63b7e3b6d366acd8263d1b608b066c3c870
Daryl <daryl@Daryls-MBP.lan>
2020-05-25 18:04:00 +0100

    new-files

--- main.tf
+++ main.tf
@@ -249,7 +249,7 @@ resource "aws_security_group" "wp_dev_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.localip}"]
   }
 
   #HTTP
